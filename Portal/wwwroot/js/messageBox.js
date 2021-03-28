c5c15c437189f55b9d44a30893807ab5a4556f16
Kovi98 <pekar.jan98@gmail.com>
2021-03-28 18:44:53 +0200

    alerts

---
+++ Portal/wwwroot/js/messageBox.js
@@ -0,0 +1,42 @@
+﻿function createError(message) {
+    var parent = $('#error-alert');
+    var div = $('<div/>', {
+        class: 'alert alert-danger alert-dismissible fade show',
+        "role": 'alert'
+    });
+    var button = $('<button></button>', {
+        class: 'close',
+        "type": 'button',
+        "data-dismiss": 'alert',
+        "aria-label": 'Close'
+    });
+    parent.append(div.append(message).append(button.append($('<span aria-hidden="true">&times;</span>'))))
+}
+function createWarning(message) {
+    var parent = $('#warning-alert');
+    var div = $('<div/>', {
+        class: 'alert alert-warning alert-dismissible fade show',
+        "role": 'alert'
+    });
+    var button = $('<button></button>', {
+        class: 'close',
+        "type": 'button',
+        "data-dismiss": 'alert',
+        "aria-label": 'Close'
+    });
+    parent.append(div.append(message).append(button.append($('<span aria-hidden="true">&times;</span>'))))
+}
+function createSuccess(message) {
+    var parent = $('#success-alert');
+    var div = $('<div/>', {
+        class: 'alert alert-success alert-dismissible fade show',
+        "role": 'alert'
+    });
+    var button = $('<button></button>', {
+        class: 'close',
+        "type": 'button',
+        "data-dismiss": 'alert',
+        "aria-label": 'Close'
+    });
+    parent.append(div.append(message).append(button.append($('<span aria-hidden="true">&times;</span>'))))
+}
