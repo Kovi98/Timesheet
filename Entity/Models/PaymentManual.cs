c3e722f3ee001c63981414e05594cfef12e083bd
Kovi98 <pekar.jan98@gmail.com>
2021-03-30 13:50:28 +0200

    partials

---
+++ Entity/Models/PaymentManual.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Timesheet.Entity.Entities
+{
+    public partial class Payment
+    {
+        public bool IsPayed { get { return !(PaymentDateTime is null); } }
+    }
+}
---
+++ Entity/Models/PersonManual.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Timesheet.Entity.Entities
+{
+    public partial class Person
+    {
+        public string FullName { get { return Name + " " + Surname; } }
+    }
+}
