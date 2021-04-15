c2c054fe895aabde39cf8f48449f8c278bd18062
Kovi98 <pekar.jan98@gmail.com>
2021-04-15 09:41:45 +0200

    platba - lokalizace, sjednocení bo

--- Entity/Entities/Payment.cs
+++ Entity/Entities/Payment.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 // Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
 // If you have enabled NRTs for your project, then un-comment the following line:
@@ -15,12 +16,40 @@ namespace Timesheet.Entity.Entities
         }
 
         public int Id { get; set; }
+
+        [Display(Name = "Vytvořeno", Description = "Datum a čas vytvoření záznamu")]
         public DateTime CreateTime { get; set; }
+
+        [Display(Name = "Datum platby", Description = "Datum a čas platby")]
         public DateTime? PaymentDateTime { get; set; }
+
+        [Display(Name = "K platbě", Description = "Určeno k platbě?")]
         public bool ToPay { get; set; }
+
+        [Display(Name = "Platební příkaz")]
         public string PaymentXml { get; set; }
         public byte[] RowVersion { get; set; }
 
+        [Display(Name = "Výkazy práce")]
         public virtual ICollection<Timesheet> Timesheet { get; set; }
+
+        //Ručně přidáno
+        [Display(Name = "Zaplaceno", Description = "Je platba zaplacena?")]
+        public bool IsPaid { get { return !(PaymentDateTime is null); } }
+
+        [Display(Name = "K vyplacení", Description = "Odměna k vyplacení")]
+        [DataType(DataType.Currency)]
+        public decimal RewardToPay
+        {
+            get
+            {
+                decimal value = 0;
+                foreach (var item in Timesheet)
+                {
+                    value += item.Reward ?? 0;
+                }
+                return value;
+            }
+        }
     }
 }
