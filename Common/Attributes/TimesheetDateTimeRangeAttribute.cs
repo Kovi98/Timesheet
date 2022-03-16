ec853ab1886ff07e6c5452b8863a18cdf6e41f2c
Jan PEKAŘ <jpekar@algotech.cz>
2022-03-16 20:23:29 +0100

    validation of timesheet datetime from and to

--- Common/Attributes/TimesheetDateTimeRangeAttribute.cs
+++ Common/Attributes/TimesheetDateTimeRangeAttribute.cs
@@ -1,43 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
 using System.ComponentModel.DataAnnotations;
 using System.Globalization;
-using System.Web.Mvc;
 
 namespace Timesheet.Common.Attributes
 {
-    public class TimesheetDateTimeRangeAttribute : ValidationAttribute, IClientValidatable
+    public class TimesheetDateTimeRangeAttribute : ValidationAttribute, IClientModelValidator
     {
-        private readonly string propertyNameToCheck;
+        private readonly string _otherProperty;
 
-        public TimesheetDateTimeRangeAttribute(string propertyNameToCheck)
+        public TimesheetDateTimeRangeAttribute(string propertyValueToCheck)
         {
-            this.propertyNameToCheck = propertyNameToCheck;
+            this._otherProperty = propertyValueToCheck;
+            ErrorMessage = "Čas Do nemůže být menší než čas Od";
         }
 
-        public IEnumerable<ModelClientValidationRule> GetClientValidationRules(ModelMetadata metadata, ControllerContext context)
+        public void AddValidation(ClientModelValidationContext context)
         {
-            var rule = new ModelClientValidationRule
-            {
-                ErrorMessage = "Čas Do nemůže být menší než čas Od",
-                ValidationType = "TimesheetDateTimeRange"
-            };
-            rule.ValidationParameters.Add("valuetocheck", propertyNameToCheck);
-            yield return rule;
+            context.Attributes.Add("data-val", "true");
+            context.Attributes.Add("data-val-datetimerange", ErrorMessage);
+            context.Attributes.Add("data-val-datetimerange-otherproperty", $"TimesheetDetail_{_otherProperty}");
         }
 
         protected override ValidationResult IsValid(object value, ValidationContext validationContext)
         {
-            var propertyName = validationContext.ObjectType.GetProperty(propertyNameToCheck);
+            var propertyName = validationContext.ObjectType.GetProperty(_otherProperty);
             if (propertyName == null)
-                return new ValidationResult(string.Format(CultureInfo.CurrentCulture, "Unknown property {0}", new[] { propertyNameToCheck }));
+                return new ValidationResult(string.Format(CultureInfo.CurrentCulture, "Unknown property {0}", new[] { _otherProperty }));
 
             var propertyValue = propertyName.GetValue(validationContext.ObjectInstance, null) as DateTime?;
 
             var isNotNull = value != null && propertyValue != null;
 
             if (isNotNull && ((DateTime?)value < propertyValue))
-                return new ValidationResult("Čas Do nemůže být menší než čas Od");
+                return new ValidationResult(ErrorMessage);
 
             return ValidationResult.Success;
         }
